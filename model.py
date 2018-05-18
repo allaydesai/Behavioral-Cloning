823978038bbfc6528b4c17c10601e30a9d096909
Allay Desai <allaydesai@gmail.com>
2018-05-18 15:09:27 -0400

    updating final model version for review added files showcasing performance

--- model.py
+++ model.py
@@ -10,35 +10,49 @@ import pickle
 import tensorflow as tf
 import numpy as np
 import cv2
-from keras.layers import Input, Flatten, Dense, Lambda
+from keras.layers import Input, Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.models import Sequential
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 from sklearn.model_selection import train_test_split
 import os, sys
 import settings
-
+import matplotlib.pyplot as plt
+import sklearn
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.regularizers import l2
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 
-# define flags:
-#flags = tf.app.flags
-#FLAGS = flags.FLAGS
-
-#flags.DEFINE_string('data_path', '', 'Path to data folder')
-#flags.DEFINE_string('training_file', '', 'Pickle file of training data')
-#flags.DEFINE_string('validation_file', '', 'Pickle file of validation data')
-#flags.DEFINE_integer('epochs', 1, 'Number of epochs')
-
-
 # Load file paths
-def LoadRawData(path):
+def LoadRawData(path, path_to_img=""):
     with open(path+'driving_log.csv') as csvfile:
         reader = csv.reader(csvfile)
         lines = [line for line in reader]
     # load data: images and measurements
-    images = np.array([cv2.imread(line[0]) for line in lines])
+    images = np.array([plt.imread(path+path_to_img+line[0].split('\\')[-1]) for line in lines])
     measurements = np.array([float(line[3]) for line in lines])
     return images, measurements
+
+def LoadAllRawData(path, path_to_img):
+    filenames = [filename for filename in os.listdir(settings.DATA_PATH)]
+    images = []
+    measurements = []
+    for filename in filenames:
+        if filename == 'DATA_0-Original' or filename == 'DATA_1-CounterClockwise' or filename == 'DATA_3-turns':
+        # if filename != 'DATA_2-path2':
+            with open(path+filename+'/'+'driving_log.csv') as csvfile:
+                reader = csv.reader(csvfile)
+                lines = [line for line in reader]
+            # load data: images and measurements
+            for line in lines:
+                image = plt.imread(path+filename+'/'+path_to_img+line[0].split('\\')[-1])
+                measurment = float(line[3])
+                images.append(image)
+                measurements.append(measurment)
+            print(filename, ' Data_size: ',len(images))
+    return np.array(images), np.array(measurements)
 
 def PrintStats():
     n_train = X_train.shape[0]
@@ -48,7 +62,7 @@ def PrintStats():
     n_classes, counts = np.unique(y_train, return_counts=True)
 
     print("Number of training examples =", n_train)
-    #print("Number of validation examples =", n_val)
+    #print("Number of validation examples =", n_val) NOTE: 20% Split in the fit function
     print("Number of testing examples =", n_test)
     print("Image data shape =", image_shape)
     print("Labels data shape =", y_train.shape)
@@ -81,45 +95,119 @@ def SaveDataToPickleFile():
     except Exception as e:
         print('Unable to save data to', settings.DATA_PICKLE_FILENAME, ':', e)
         raise
+        
+def DataAugmentation(images, measurements):
+    # Flip images and negate steering
+    aug_imgs, aug_measure = [], []
+    for image, measurement in zip(images, measurements):
+        aug_imgs.append(image)
+        aug_measure.append(measurement)
+        aug_imgs.append(cv2.flip(image,1))
+        aug_measure.append(measurement * -1.0)
+    return np.array(aug_imgs), np.array(aug_measure)
+        
+def CreateModel():
+    model = Sequential()
+    model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((70,25), (0,0))))
+    model.add(Convolution2D(6,5,5, activation="relu"))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(6,5,5, activation="relu"))
+    model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dense(84)) 
+    model.add(Dense(1)) 
+    return model
     
-#def main(_):
-# load data
+def CreateNvidiaModel():
+    model = Sequential()
+    model.add(Lambda((lambda x: x/255.0 - 0.5), input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((65,25), (0,0))))
+    model.add(Convolution2D(24,5,5, subsample=(2,2), W_regularizer=l2(0.001), activation="relu"))
+    model.add(Convolution2D(36,5,5, subsample=(2,2), W_regularizer=l2(0.001), activation="relu"))
+    model.add(Convolution2D(48,5,5, subsample=(2,2), W_regularizer=l2(0.001), activation="relu"))
+    model.add(Convolution2D(64,3,3, W_regularizer=l2(0.001), activation="relu"))
+    model.add(Convolution2D(64,3,3, W_regularizer=l2(0.001), activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(100, W_regularizer=l2(0.001), activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(50, W_regularizer=l2(0.001), activation="relu")) 
+    model.add(Dropout(0.3))
+    model.add(Dense(10, W_regularizer=l2(0.001), activation="relu")) 
+    model.add(Dropout(0.3))
+    model.add(Dense(1)) 
+    return model
+
+def CreateNvidiaOldModel():
+    model = Sequential()
+    model.add(Lambda((lambda x: x/255.0 - 0.5), input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((65,25), (0,0))))
+    model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
+    model.add(Convolution2D(64,3,3, activation="relu"))
+    model.add(Convolution2D(64,3,3, activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(100, W_regularizer=l2(0.001)))
+    model.add(Dropout(0.3))
+    model.add(Dense(50, W_regularizer=l2(0.001))) 
+    model.add(Dropout(0.3))
+    model.add(Dense(10, W_regularizer=l2(0.001))) 
+    model.add(Dropout(0.3))
+    model.add(Dense(1)) 
+    return model
+	
+###############################################
+#------------------- MAIN --------------------#
+###############################################
+
+# LOAD DATA
 #check if pickle file exists
 if os.path.isfile(settings.DATA_PATH + settings.DATA_PICKLE_FILENAME):
     X_train, y_train, X_test, y_test = LoadFromPickleFile()
 else:
-    X_train, y_train = LoadRawData(settings.DATA_PATH)#FLAGS.data_path)
-    # Split data 
-    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.15, random_state=0)
+    #load raw data
+    X_train, y_train = LoadAllRawData(settings.DATA_PATH, settings.PATH_TO_IMG)
+    #shuffle
+    sklearn.utils.shuffle(X_train, y_train)
+    # Split data - NOTE: not splitting any test data, using the simulator to test instead
+    #X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.15, random_state=0)
+    X_test = np.array([])
+    y_test = np.array([])
     # Create pickle file for future use
     SaveDataToPickleFile()
     print('Data cached in pickle file.')
 
+# AUGMENT DATA
+X_train, y_train = DataAugmentation(X_train, y_train)
+
 PrintStats()
 
-# pre-process data
 
-# define model
-print('Defining Model')
-model = Sequential()
-model.add(Lambda(lambda x: x/255 - 0.5, input_shape=(160,320,3)))
-model.add(Flatten())
-model.add(Dense(1)) 
+# DEFINE MODEL
+model = CreateNvidiaOldModel()
 
 model.compile(loss='mse', optimizer='adam',  metrics=['accuracy'])
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs = settings.EPOCHS)
+
+# TRAINING
+# define callbacks
+checkpoint = ModelCheckpoint(filepath=settings.MODEL_FILENAME,verbose=0, save_best_only=True, mode=min, monitor='val_loss')
+earlyStopping = EarlyStopping(monitor='val_loss', patience=2,verbose=0,mode='min')
+callbacks = [checkpoint, earlyStopping]
+
+history = model.fit(X_train, y_train, batch_size = settings.BATCH_SIZE, validation_split=0.2, shuffle=True, epochs=settings.EPOCHS, callbacks=callbacks)
 
 print('Saving model...')
 model.save(settings.MODEL_FILENAME)
 
-print("Testing...")
-metrics = model.evaluate(X_test, y_test)
-
-for metric_i in range(len(model.metrics_names)):
-    metric_name = model.metrics_names[metric_i]
-    metric_value = metrics[metric_i]
-    print('{}: {}'.format(metric_name, metric_value))
-    
-# parses flags and calls the `main` function above
-#if __name__ == '__main__':
-#    tf.app.run()
+# Visualizing loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training_set', 'validation_set'], loc='upper right')
+plt.savefig('loss_plot.png')
+print('Plot Saved as loss_plot.png..')
+plt.show()
--- settings.py
+++ settings.py
@@ -5,10 +5,18 @@ Created on Sat May 12 16:33:47 2018
 @author: Allay
 """
 
-DATA_PATH = './data_new/' #'./data/data/' <OR> './data_new/'
+DATA_PATH = '/media/user21st/Allay-usb/DATA/'#'F:\\DATA\\'
 
-DATA_PICKLE_FILENAME = 'data.pickle'
+DATA_PICKLE_FILENAME = 'data_4.pickle'
 
-MODEL_FILENAME = 'model.h5'
+MODEL_FILENAME = 'model_4_4_epoch.h5'
 
-EPOCHS = 100
+EPOCHS = 4
+
+BATCH_SIZE = 128
+
+STEERING_CORRECTION = 0.2
+
+#PATH_TO_IMG = '/media/user21st/Allay-usb/Udacity/SelfDrivingCar/CarND-Behavioral-Cloning-P3-master/CarND-Behavioral-Cloning-P3-master/data_new/IMG/'
+PATH_TO_IMG = "IMG\\"
+
